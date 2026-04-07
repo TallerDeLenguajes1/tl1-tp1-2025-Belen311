1fa0a576ee3944517a0a618c73ab7aa164c5a480
Beluluuu <sofibelengerman@gmail.com>
2026-04-07 02:00:59 -0300

    Contenido del puntero

--- tp1_1.c
+++ tp1_1.c
@@ -4,5 +4,14 @@ int main(){
     
     printf("Hola mundo\n");
 
+    int num;
+    int *pnum = &num;
+
+    printf("Contenido del puntero: %d\n", *pnum);
+    printf("Direccion de memoria almacenada por el puntero: %p\n", pnum);
+    printf("Direccion de memoria de la variable: %p\n", &num);
+    printf("Direccion de memoria de puntero: %p\n", &pnum);
+    printf("Tamanio de memoria utilizado por la variable: %d\n", sizeof(num));
+
     return 0;
 }
